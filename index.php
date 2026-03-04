60d44508a54013a1d12503b15e9053b3477cb33e
Lumiiiiii <lumiburroni@gmail.com>
2026-03-04 11:58:29 +0100

    corretta icona dei medicinali

--- index.php
+++ index.php
@@ -270,7 +270,8 @@ $totalPatients = $patientManager->countPatients();
             <a href="medicinali_gestione.php">
                 <!-- Icona SVG: capsula / medicina -->
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M9 3v2m6-2v2M9 19v2m6-2v2M5 9H3m2 6H3m18-6h-2m2 6h-2M7 19h10a2 2 0 002-2V7a2 2 0 00-2-2H7a2 2 0 00-2 2v10a2 2 0 002 2zM9 9h6v6H9V9z" />
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 20.5l10-10a4.95 4.95 0 10-7-7l-10 10a4.95 4.95 0 107 7z" />
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M8.5 8.5l7 7" />
                 </svg>
                 Medicinali
             </a>
